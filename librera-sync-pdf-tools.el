1f31ad212d806051bbb2782800a4366f24cf0e78
Dmitriy P <jumper047@gmail.com>
2021-04-11 03:04:09 +0300

    Add separate plugin for pdf-tools

---
+++ librera-sync-pdf-tools.el
@@ -0,0 +1,39 @@
+(require 'dash)
+(require 'f)
+
+(defvar librera-sync-supported-modes '(pdf-view-mode)
+  "Major modes supported by librera.")
+
+(declare-function librera-sync-save "librera-sync" ())
+
+(declare-function pdf-view-goto-page "pdf-view" (page))
+(declare-function pdf-cache-number-of-pages "pdf-cache" ())
+
+;;; PDF-tools
+
+(defun librera-sync--pdf-view-mode-prepare ()
+  "Add librera hooks to PDF buffer."
+  (add-hook 'pdf-view-after-change-page-hook 'librera-sync-save 0 't)
+)
+
+(defun librera-sync--pdf-view-mode-clean ()
+  "Remove librera hooks from PDF buffer."
+  (remove-hook 'pdf-view-after-change-page-hook 'librera-sync-save 't)
+)
+
+(defun librera-sync--pdf-view-mode-set-pos (position)
+  "Set POSITION in pdf-view buffer."
+  (pdf-view-goto-page
+   (round (* position (pdf-cache-number-of-pages)))))
+
+(defun librera-sync--pdf-view-mode-current-pos ()
+  "Get current position in pdf-view buffer."
+  (/ (eval `(pdf-view-current-page))
+     (float (pdf-cache-number-of-pages))))
+
+(defun librera-sync--pdf-view-book-name ()
+  "Get current book name."
+  (f-filename (buffer-file-name))
+  )
+ 
+(provide 'librera-sync-modes)
